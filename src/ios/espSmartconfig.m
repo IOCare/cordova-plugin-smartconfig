004d1a2226ec4307d561e9602983987722f4c46c
Timothy Wu <g39328006@ym.edu.tw>
2018-01-17 16:24:41 +0800

    Smart config result pass back ip address of the device

--- src/ios/espSmartconfig.m
+++ src/ios/espSmartconfig.m
@@ -23,77 +23,86 @@
 @implementation espSmartconfig
 
 - (void) startConfig:(CDVInvokedUrlCommand *)command{
-    [self._condition lock];
-    NSString *apSsid = (NSString *)[command.arguments objectAtIndex:0];
-    NSString *apBssid = (NSString *)[command.arguments objectAtIndex:1];
-    NSString *apPwd = (NSString *)[command.arguments objectAtIndex:2];
-    NSString *isSsidHiddenStr=(NSString *)[command.arguments objectAtIndex:3];
-    
-    BOOL isSsidHidden = true;
-    if([isSsidHiddenStr compare:@"NO"]==NSOrderedSame){
-        isSsidHidden=false;
-    }
-    int taskCount = (int)[command.arguments objectAtIndex:4];
-    self._esptouchTask =
-    [[ESPTouchTask alloc]initWithApSsid:apSsid andApBssid:apBssid andApPwd:apPwd andIsSsidHiden:isSsidHidden];
-    EspTouchDelegateImpl *esptouchDelegate=[[EspTouchDelegateImpl alloc]init];
-    esptouchDelegate.command=command;
-    esptouchDelegate.commandDelegate=self.commandDelegate;
-    [self._esptouchTask setEsptouchDelegate:esptouchDelegate];
-    [self._condition unlock];
-    NSArray * esptouchResultArray = [self._esptouchTask executeForResults:taskCount];
     [self.commandDelegate runInBackground:^{
-    dispatch_queue_t  queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(queue, ^{
-        // show the result to the user in UI Main Thread
-        dispatch_async(dispatch_get_main_queue(), ^{
-
-            
-            ESPTouchResult *firstResult = [esptouchResultArray objectAtIndex:0];
-            // check whether the task is cancelled and no results received
-            if (!firstResult.isCancelled)
-            {
-                //NSMutableString *mutableStr = [[NSMutableString alloc]init];
-                //NSUInteger count = 0;
-                // max results to be displayed, if it is more than maxDisplayCount,
-                // just show the count of redundant ones
-                //const int maxDisplayCount = 5;
-                if ([firstResult isSuc])
+        dispatch_queue_t  queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        
+        [self._condition lock];
+        NSString *apSsid = (NSString *)[command.arguments objectAtIndex:0];
+        NSString *apBssid = (NSString *)[command.arguments objectAtIndex:1];
+        NSString *apPwd = (NSString *)[command.arguments objectAtIndex:2];
+        NSString *isSsidHiddenStr=(NSString *)[command.arguments objectAtIndex:3];
+        
+        BOOL isSsidHidden = true;
+        if([isSsidHiddenStr compare:@"NO"]==NSOrderedSame){
+            isSsidHidden=false;
+        }
+        int taskCount = (int)[command.arguments objectAtIndex:4];
+        
+        NSLog(@"ssid: %@, bssid: %@, apPwd: %@", apSsid, apBssid, apPwd);
+        self._esptouchTask =
+        [[ESPTouchTask alloc]initWithApSsid:apSsid andApBssid:apBssid andApPwd:apPwd andIsSsidHiden:isSsidHidden];
+        EspTouchDelegateImpl *esptouchDelegate=[[EspTouchDelegateImpl alloc]init];
+        esptouchDelegate.command=command;
+        esptouchDelegate.commandDelegate=self.commandDelegate;
+        [self._esptouchTask setEsptouchDelegate:esptouchDelegate];
+        [self._condition unlock];
+        NSArray * esptouchResultArray = [self._esptouchTask executeForResults:taskCount];
+        
+        dispatch_async(queue, ^{
+            // show the result to the user in UI Main Thread
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                
+                ESPTouchResult *firstResult = [esptouchResultArray objectAtIndex:0];
+                // check whether the task is cancelled and no results received
+                if (!firstResult.isCancelled)
                 {
+                    //NSMutableString *mutableStr = [[NSMutableString alloc]init];
+                    //NSUInteger count = 0;
+                    // max results to be displayed, if it is more than maxDisplayCount,
+                    // just show the count of redundant ones
+                    //const int maxDisplayCount = 5;
+                    if ([firstResult isSuc])
+                    {
+                        
+                        //                    for (int i = 0; i < [esptouchResultArray count]; ++i)
+                        //                    {
+                        //                        ESPTouchResult *resultInArray = [esptouchResultArray objectAtIndex:i];
+                        //                        [mutableStr appendString:[resultInArray description]];
+                        //                        [mutableStr appendString:@"\n"];
+                        //                        count++;
+                        //                        if (count >= maxDisplayCount)
+                        //                        {
+                        //                            break;
+                        //                        }
+                        //                    }
+                        //
+                        //                    if (count < [esptouchResultArray count])
+                        //                    {
+                        //                        [mutableStr appendString:[NSString stringWithFormat:@"\nthere's %lu more result(s) without showing\n",(unsigned long)([esptouchResultArray count] - count)]];
+                        //                    }
+                        
+                        ESPTouchResult *resultInArray = [esptouchResultArray objectAtIndex:0];
+                        NSString *ipaddr = [ESP_NetUtil descriptionInetAddr4ByData:resultInArray.ipAddrData];
+                        NSDictionary* returnObj = @{@"ipaddr": ipaddr};
+                        CDVPluginResult* pluginResult = nil;
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:returnObj];
+                        
+                        [pluginResult setKeepCallbackAsBool:true];
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                    }
                     
-//                    for (int i = 0; i < [esptouchResultArray count]; ++i)
-//                    {
-//                        ESPTouchResult *resultInArray = [esptouchResultArray objectAtIndex:i];
-//                        [mutableStr appendString:[resultInArray description]];
-//                        [mutableStr appendString:@"\n"];
-//                        count++;
-//                        if (count >= maxDisplayCount)
-//                        {
-//                            break;
-//                        }
-//                    }
-//                    
-//                    if (count < [esptouchResultArray count])
-//                    {
-//                        [mutableStr appendString:[NSString stringWithFormat:@"\nthere's %lu more result(s) without showing\n",(unsigned long)([esptouchResultArray count] - count)]];
-//                    }
-                    CDVPluginResult* pluginResult = nil;
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"finished"];
-                    [pluginResult setKeepCallbackAsBool:true];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                    else
+                    {
+                        CDVPluginResult* pluginResult = nil;
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"Esptouch fail"];
+                        [pluginResult setKeepCallbackAsBool:true];
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                    }
                 }
                 
-                else
-                {
-                    CDVPluginResult* pluginResult = nil;
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"Esptouch fail"];
-                    [pluginResult setKeepCallbackAsBool:true];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                }
-            }
-            
+            });
         });
-    });
     }];
 }
 
